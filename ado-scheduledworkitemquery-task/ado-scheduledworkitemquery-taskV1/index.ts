da314c6dd66b782e6edadd9b8e600e32ac109334
Michel Zehnder <MichelZ@users.noreply.github.com>
2020-02-13 15:52:27 +0100

    Fix issue with sorting

--- ado-scheduledworkitemquery-task/ado-scheduledworkitemquery-taskV1/index.ts
+++ ado-scheduledworkitemquery-task/ado-scheduledworkitemquery-taskV1/index.ts
@@ -361,7 +361,33 @@ async function run() {
             const referenceName = field.referenceName!;
             tl.debug("Ordering by " + referenceName + (sortColumn.descending! ? " descending" : " ascending"));
 
-            workItems.sort((a, b) => (a.fields![referenceName].localeCompare(b.fields![referenceName])));
+            workItems.sort((a, b) => 
+                {
+                    tl.debug("Processing field [" + referenceName + "]");
+                    let fieldValueA = a.fields![referenceName];
+                    let fieldValueB = b.fields![referenceName];
+
+                    if (fieldValueA === undefined)
+                    {
+                        return 1;
+                    }
+
+                    if (fieldValueB === undefined)
+                    {
+                        return -1;
+                    }
+
+                    if (typeof(fieldValueA) === "string" && typeof(fieldValueB) === "string") {
+                        return fieldValueA.localeCompare(fieldValueB);
+                    } else if (typeof(fieldValueA) === "number" && (typeof(fieldValueB) === "number")){
+                        return fieldValueA - fieldValueB;
+                    } else if (typeof(fieldValueA) === "object" && fieldValueA.hasOwnProperty("displayName")) {
+                            return fieldValueA.displayName.localeCompare(fieldValueB.displayName);
+                    } else {
+                        return fieldValueA > fieldValueB ? 1 : -1;
+                    }
+                }
+            );
             
             if (sortColumn.descending)
             {
